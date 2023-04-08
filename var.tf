d347d4251e7b8f7897de92e4db510b413e5fc84a
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-08 18:00:08 +0000

    yes

--- var.tf
+++ var.tf
@@ -2,6 +2,10 @@ variable "env" {}
 variable "component" {}
 variable "instance_type" {}
 variable "tags" {}
-
+variable "desired_capacity" {}
+variable "max_size" {}        
+variable "min_size" {}  
+variable "subnets"  {}
+  
 
 
