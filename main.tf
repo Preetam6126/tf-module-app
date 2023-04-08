b3f0eaf4aa334785e3dd535404284e7481b18059
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-08 18:10:13 +0000

    hell

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "aws_launch_template" "main" {
 }
 
 resource "aws_autoscaling_group" "main" {
+  name               = "${var.component}-${var.env}"
   desired_capacity   = var.desired_capacity
   max_size           = var.max_size
   min_size           = var.min_size
@@ -32,5 +33,14 @@ resource "aws_autoscaling_group" "main" {
   launch_template {
     id      = aws_launch_template.main.id
     version = "$Latest"
-  }
+    }
+tag {
+   
+ key                 = "Name"
+ propagate_at_launch = false
+ value               = "${var.component}-${var.env}"
+  
+  
+   }
+  
 }
